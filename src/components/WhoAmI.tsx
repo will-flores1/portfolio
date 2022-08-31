efe0921732b81739ce0cb69acb394a668372bc20
wilfredo-flores <willflores1221@gmail.com>
2022-08-31 08:57:51 -0400

    Fix project component

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -3,12 +3,13 @@ import pp from "../assets/profile-picture.png";
 import github from "../assets/github.svg";
 import twitter from "../assets/twitter.svg";
 import linkedin from "../assets/linkedin.svg";
+import ProjectTemplate from "./ProjectTemplate";
 
 function WhoAmI() {
 	return (
 		<div className="flex flex-col mx-auto max-w-3xl lg:max-w-3xl px-16 lg:px-20">
 			<div className="bg-white/30 dark:bg-white/50 px-10 py-3 rounded-md mt-10 text-sm text-center text-black/70 tracking-wide dark:text-white">
-				Hi, I'm a web developer based in Maryland
+				Currently studying data structures
 			</div>
 			<div className="lg:flex lg:justify-between">
 				<div>
@@ -16,7 +17,7 @@ function WhoAmI() {
 						Wilfredo Flores
 					</div>
 					<div className="text-black/70 dark:text-white">
-						Digital Craftsmen ( Developer / Designer )
+						( Software / Web ) Developer
 					</div>
 				</div>
 				<img
@@ -29,10 +30,7 @@ function WhoAmI() {
 				Me in 10 seconds
 			</div>
 			<div className="mt-2 font-extralight dark:text-white">
-				I am web developer / designer based in Maryland
-			</div>
-			<div className="mt-2 font-extralight dark:text-white">
-				I've taken{" "}
+				I've learned web development from{" "}
 				<a
 					href="https://www.edx.org/course/cs50s-web-programming-with-python-and-javascript"
 					target="_blank"
@@ -55,107 +53,83 @@ function WhoAmI() {
 					rel="noreferrer"
 					className="font-black hover:underline"
 				>
-					, Web Design
+					, & Web Design
 				</a>
 			</div>
 			<div className="mt-2 font-extralight dark:text-white">
-				I build websites using React, Javascript, and Typescript
+				I build web apps using React
 			</div>
 			<div className="mt-2 font-extralight dark:text-white">
-				I speak english and spanish
+				I speak English (native) and Spanish (proficient)
 			</div>
 			<div className="mt-2 font-extralight dark:text-white">
-				<strong>Things that fire me up:</strong> health, productivity,
-				computers, photography, and movies
+				<strong>Things that fire me up:</strong> soccer, programming, gaming,
+				photography
 			</div>
-			<a
+			{/* <a
 				href="https://www.wilfredoflores.dev/"
 				className="bg-emerald-600 w-fit mx-auto mt-6 py-3 px-6 rounded-md text-white hover:cursor-pointer hover:bg-emerald-700 transition ease-in-out duration-150 font-bold"
 				target="_blank"
 				rel="noreferrer"
 			>
 				My portfolio
-			</a>
+			</a> */}
 			<div className="mt-6 font-semibold text-xl border-black border-b-4 w-fit pb-1 tracking-wider dark:text-white dark:border-white">
 				Projects
 			</div>
-			<div className="py-3 overflow-normal dark:text-white/90">
-				<a
-					className="font-bold pr-3 text-blue-500 hover:text-black dark:hover:text-white transition duration-150 ease-in-out"
-					target="_blank"
-					rel="noreferrer"
-					href="https://twitter-2-0-nu.vercel.app/"
-				>
-					Twitter Clone
-				</a>{" "}
-				Tweet, Sign In, Comment functionality. Built w/ Next.js, React,
-				Typescript, Sanity, GROQ, Tailwindcss{" "}
-				<a
-					href="https://twitter-2-0-nu.vercel.app/"
-					target="_blank"
-					rel="noreferrer"
-					className="pl-3 font-bold text-blue-500 hover:underline"
-				>
-					Live
-				</a>
-				<a
-					href="https://github.com/will-flores1/twitter-2.0"
-					target="_blank"
-					rel="noreferrer"
-					className="pl-3 font-bold text-blue-500 hover:underline"
-				>
-					Source
-				</a>
-			</div>
-			<div className="py-2 overflow-normal dark:text-white/90 mb-2">
-				<a
-					className="font-bold pr-3 text-blue-500 hover:text-black dark:hover:text-white transition duration-150 ease-in-out"
-					target="_blank"
-					rel="noreferrer"
-					href="https://twitter-2-0-nu.vercel.app/"
-				>
-					Medium Clone
-				</a>{" "}
-				Post & Comment functionality. Front-end practice. Built w/ Next.js,
-				React, Typescript, Sanity, GROQ, Tailwindcss{" "}
-				<a
-					href="https://twitter-2-0-nu.vercel.app/"
-					target="_blank"
-					rel="noreferrer"
-					className="pl-3 text-blue-500 hover:underline font-black"
-				>
-					Live
-				</a>
-				<a
-					href="https://github.com/will-flores1/twitter-2.0"
-					target="_blank"
-					rel="noreferrer"
-					className="p-3 text-blue-500 hover:underline font-black"
-				>
-					Source
-				</a>
-			</div>
+			{/* Twitter */}
+			<ProjectTemplate
+				title="Twitter Timeline"
+				description="Tweet, Sign In, Comment functionality. Built w/ Next.js, React,
+				Typescript, Sanity, GROQ, Tailwindcss"
+				demoLink="https://twitter-2-0-nu.vercel.app/"
+				githubLink="https://github.com/will-flores1/twitter-2.0"
+			/>
+			{/* Tenzies */}
+			<ProjectTemplate
+				title="Tenzies Game"
+				description="The objective is to get all dice on the same number. Built w/ Javascript & React. "
+				demoLink="https://tenzies-game-olive.vercel.app/"
+				githubLink="https://github.com/will-flores1/tenzies-game"
+			/>
+			{/* Local Library */}
+			<ProjectTemplate
+				title="Book Library"
+				description="A library wheres books are stored. Built w/ Javascript, CSS, & HTML."
+				demoLink="https://will-flores1.github.io/library-local/"
+				githubLink="https://github.com/will-flores1/tenzies-game"
+			/>
+			{/* Calculator */}
+			<ProjectTemplate
+				title="Calculator"
+				description="My version of the calculator for The Odin Project. Built w/ Javascript, CSS, & HTML."
+				demoLink="https://will-flores1.github.io/calculator/"
+				githubLink="https://github.com/will-flores1/calculator"
+			/>
+			{/* Rock, Paper, Scissors */}
+			<ProjectTemplate
+				title="Rock Paper Scissors"
+				description="My version of RPS for The Odin Project. Built w/ Javascript, CSS, & HTML."
+				demoLink="https://will-flores1.github.io/rock-paper-scissors/"
+				githubLink="https://github.com/wilfredo-flores/rock-paper-scissors"
+			/>
+
 			<div>
 				<div className="mt-6 font-semibold text-xl border-black border-b-4 w-fit pb-1 tracking-wide dark:text-white dark:border-white tracking-normal">
 					On the web
 				</div>
-				<a
-					href="https://github.com/will-flores1"
-					target="_blank"
-					rel="noreferrer"
-					className="mt-3 flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit"
-				>
-					<img src={github} alt="" className="h-6 w-6 fill-current pr-2" />
-					<a
-						href="https://github.com/will-flores1"
-						target="_blank"
-						rel="noreferrer"
-						className="font-bold text-moonTeal hover:underline"
-					>
-						<span className="font-light">@</span>will-flores1
-					</a>
-				</a>
 			</div>
+			<a
+				href="https://github.com/will-flores1"
+				target="_blank"
+				rel="noreferrer"
+				className="mt-3 flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit"
+			>
+				<img src={github} alt="" className="h-6 w-6 fill-current pr-2" />
+				<div className="font-bold text-moonTeal hover:underline">
+					<span className="font-light">@</span>will-flores1
+				</div>
+			</a>
 			<a
 				href="https://twitter.com/will_flores1"
 				target="_blank"
@@ -163,14 +137,9 @@ function WhoAmI() {
 				className="flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit"
 			>
 				<img src={twitter} alt="" className="h-6 w-6 fill-current pr-2" />
-				<a
-					href="https://twitter.com/will_flores1"
-					target="_blank"
-					rel="noreferrer"
-					className="font-bold text-moonTeal hover:underline"
-				>
+				<div className="font-bold text-moonTeal hover:underline">
 					<span className="font-light">@</span>will_flores1
-				</a>
+				</div>
 			</a>
 			<a
 				href="https://www.linkedin.com/in/wilfredo-flores1/"
@@ -179,14 +148,9 @@ function WhoAmI() {
 				className="flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit mb-10"
 			>
 				<img src={linkedin} alt="" className="h-6 w-6 fill-current pr-2" />
-				<a
-					href="https://www.linkedin.com/in/wilfredo-flores1/"
-					target="_blank"
-					rel="noreferrer"
-					className="font-bold text-moonTeal hover:underline"
-				>
+				<div className="font-bold text-moonTeal hover:underline">
 					<span className="font-light">@</span>wilfredo-flores1
-				</a>
+				</div>
 			</a>
 		</div>
 	);
