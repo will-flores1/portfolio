4efa61d949547b439a03e38a35e70d9e1c1b12ec
wilfredo-flores <willflores1221@gmail.com>
2022-08-31 08:56:39 -0400

    Add theme functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,29 @@ import Header from "./components/Header";
 import WhoAmI from "./components/WhoAmI";
 
 function App() {
+	const [theme, setTheme] = React.useState(
+		localStorage.getItem("data-theme") || "light"
+	);
+	document.documentElement.setAttribute("class", theme);
+
+	const toggleTheme = () => {
+		if (theme === "dark") {
+			localStorage.setItem("data-theme", "light");
+			document.documentElement.classList.remove("dark");
+			document.documentElement.classList.add("light");
+			setTheme("light");
+		} else {
+			localStorage.setItem("data-theme", "dark");
+			document.documentElement.classList.remove("light");
+			document.documentElement.classList.add("dark");
+			setTheme("dark");
+		}
+		return "";
+	};
+
 	return (
 		<div className="min-h-screen bg-background dark:bg-darkBackground transition duration-700 ease-in-out">
-			<Header />
+			<Header theme={theme} toggleTheme={toggleTheme} />
 			<WhoAmI />
 		</div>
 	);
