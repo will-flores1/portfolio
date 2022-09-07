0064dd8de8f6c9b4170aab83ec8a0258d8204169
wilfredo-flores <willflores1221@gmail.com>
2022-09-06 22:52:14 -0400

    Fix mobile design

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -7,17 +7,17 @@ import ProjectTemplate from "./ProjectTemplate";
 
 function WhoAmI() {
 	return (
-		<div className="flex flex-col mx-auto max-w-3xl lg:max-w-3xl px-16 lg:px-20">
+		<div className="flex flex-col mx-auto max-w-3xl lg:max-w-3xl px-10 lg:px-20">
 			{/* <div className="bg-white/30 dark:bg-white/50 px-10 py-3 rounded-md mt-10 text-sm text-center text-black/70 tracking-wide dark:text-white">
 				Hi, I am a web developer based in Maryland
 			</div> */}
 			<div className="lg:flex lg:justify-between">
 				<div>
-					<div className="mt-5 dark:text-white font-bold tracking-wider text-3xl text-black/70">
+					<div className="mt-5 dark:text-white font-bold tracking-wider lg:text-3xl text-4xl text-black/70">
 						Wilfredo Flores
 					</div>
-					<div className="text-black/70 dark:text-white">
-						( Software / Web ) Developer
+					<div className="text-black/70 dark:text-white text-lg lg:text-base">
+						Digital Craftsmen ( Developer / Designer )
 					</div>
 				</div>
 				<img
@@ -26,10 +26,10 @@ function WhoAmI() {
 					alt=""
 				/>
 			</div>
-			<div className="dark:text-white font-semibold text-xl border-black border-b-4 w-fit pb-1 tracking-wider dark:border-white">
+			<div className="dark:text-white font-semibold text-2xl lg:text-xl border-black border-b-4 w-fit pb-1 tracking-wider dark:border-white">
 				Me in 10 seconds
 			</div>
-			<div className="mt-2 font-extralight dark:text-white">
+			<div className="mt-2 font-extralight dark:text-white lg:text-base text-lg">
 				I've learned web development from{" "}
 				<a
 					href="https://www.edx.org/course/cs50s-web-programming-with-python-and-javascript"
@@ -58,10 +58,10 @@ function WhoAmI() {
 					Scrimba
 				</a>
 			</div>
-			<div className="mt-2 font-extralight dark:text-white">
+			<div className="mt-2 font-extralight dark:text-white lg:text-base text-lg">
 				I speak English (native) and Spanish (proficient)
 			</div>
-			<div className="mt-2 font-extralight dark:text-white">
+			<div className="mt-2 font-extralight dark:text-white lg:text-base text-lg">
 				<strong>Things that fire me up:</strong> soccer, gaming, photography
 			</div>
 			{/* <a
@@ -72,7 +72,7 @@ function WhoAmI() {
 			>
 				My portfolio
 			</a> */}
-			<div className="mt-6 font-semibold text-xl border-black border-b-4 w-fit pb-1 tracking-wider dark:text-white dark:border-white">
+			<div className="mt-6 font-semibold text-2xl lg:text-xl border-black border-b-4 w-fit pb-1 tracking-wider dark:text-white dark:border-white">
 				Projects
 			</div>
 			{/* Twitter */}
@@ -105,7 +105,7 @@ function WhoAmI() {
 			/>
 
 			<div>
-				<div className="mt-6 font-semibold text-xl border-black border-b-4 w-fit pb-1 tracking-wide dark:text-white dark:border-white tracking-normal">
+				<div className="mt-6 font-semibold text-2xl lg:text-xl border-black border-b-4 w-fit pb-1 tracking-wide dark:text-white dark:border-white tracking-normal">
 					On the web
 				</div>
 			</div>
@@ -113,9 +113,9 @@ function WhoAmI() {
 				href="https://github.com/will-flores1"
 				target="_blank"
 				rel="noreferrer"
-				className="mt-3 flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit"
+				className="mt-3 flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit text-lg lg:text-base"
 			>
-				<img src={github} alt="" className="h-6 w-6 fill-current pr-2" />
+				<img src={github} alt="" className="h-7 w-7 fill-current pr-2" />
 				<div className="font-bold text-moonTeal hover:underline">
 					<span className="font-light">@</span>will-flores1
 				</div>
@@ -124,9 +124,9 @@ function WhoAmI() {
 				href="https://twitter.com/will_flores1"
 				target="_blank"
 				rel="noreferrer"
-				className="flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit"
+				className="flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit text-lg lg:text-base"
 			>
-				<img src={twitter} alt="" className="h-6 w-6 fill-current pr-2" />
+				<img src={twitter} alt="" className="h-7 w-7 fill-current pr-2" />
 				<div className="font-bold text-moonTeal hover:underline">
 					<span className="font-light">@</span>will_flores1
 				</div>
@@ -135,9 +135,9 @@ function WhoAmI() {
 				href="https://www.linkedin.com/in/wilfredo-flores1/"
 				target="_blank"
 				rel="noreferrer"
-				className="flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit mb-16"
+				className="flex items-center hover:bg-blue-200 py-2 px-4 rounded-sm transition duration-500 ease-in-out w-fit mb-16 text-lg lg:text-base"
 			>
-				<img src={linkedin} alt="" className="h-6 w-6 fill-current pr-2" />
+				<img src={linkedin} alt="" className="h-7 w-7 fill-current pr-2" />
 				<div className="font-bold text-moonTeal hover:underline">
 					<span className="font-light">@</span>wilfredo-flores1
 				</div>
