4eb0fc4b00f89ab4a856fe76568b239ce48ce680
wilfredo-flores <willflores1221@gmail.com>
2022-09-11 21:37:55 -0400

    Add component

---
+++ src/components/Work.tsx
@@ -0,0 +1,20 @@
+function Work() {
+	return (
+		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 text-center dark:text-white mb-10">
+			<a href="" target="_blank" rel="noreferrer" className="">
+				<img src="" alt="thumbnail" className="rounded-xl mb-4" />
+				<p>How to build a portfolio website</p>
+			</a>
+			<a href="" target="_blank" rel="noreferrer" className="">
+				<img src="" alt="thumbnail" className="rounded-xl mb-4" />
+				<p>How to build a portfolio website</p>
+			</a>
+			<a href="" target="_blank" rel="noreferrer" className="">
+				<img src="" alt="thumbnail" className="rounded-xl mb-4" />
+				<p>How to build a portfolio website</p>
+			</a>
+		</div>
+	);
+}
+
+export default Work;
