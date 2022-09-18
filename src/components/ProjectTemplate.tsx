303d543922f57cf2bad48cf22671f02a206be921
wilfredo-flores <willflores1221@gmail.com>
2022-09-18 11:49:01 -0400

    Fix project UI

--- src/components/ProjectTemplate.tsx
+++ src/components/ProjectTemplate.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 function ProjectTemplate(props: any) {
 	return (
-		<div className="py-3 lg:py-2 overflow-normal dark:text-white/90 mb-2 lg:text-base text-lg">
-			<a
-				className="font-bold pr-3 text-blue-500 hover:text-black dark:hover:text-white transition duration-150 ease-in-out"
+		<div className=" dark:text-white/90 text-black/90">
+			<a href={props.githubLink} target="_blank" rel="noreferrer">
+				<img
+					src={props.demoImg}
+					alt="thumbnail"
+					className="rounded-xl mb-4 w-fit h-fit"
+				/>
+				<p className="text-xl md:text-lg">{props.title}</p>
+				<p className="font-light pt-1 text-base md:text-sm">
+					{props.description}
+				</p>
+			</a>
+			{/* <a
+				href={props.demoLink}
 				target="_blank"
 				rel="noreferrer"
-				href={props.demoLink}
-			>
-				{props.title}
-			</a>{" "}
-			{props.description}{" "}
-			<a
-				href={props.demoLink}
-				target="_blank"
-				rel="noreferrer"
-				className="pl-3 text-blue-500 hover:underline font-bold"
+				className="pl-3 text-blue-500 hover:underline md:font-normal lg:text-base text-lg font-semibold"
 			>
 				Live
 			</a>
@@ -24,10 +26,10 @@ function ProjectTemplate(props: any) {
 				href={props.githubLink}
 				target="_blank"
 				rel="noreferrer"
-				className="p-3 text-blue-500 hover:underline font-bold"
+				className="p-3 text-blue-500 hover:underline md:font-normal lg:text-base text-lg font-semibold"
 			>
 				Source-code
-			</a>
+			</a> */}
 		</div>
 	);
 }
