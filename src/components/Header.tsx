66ab0a71aba66ed5a69cb69703cd3ee7665d7ad3
wilfredo-flores <willflores1221@gmail.com>
2022-09-18 11:48:47 -0400

    Optimize img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import paw from "../assets/footprint.webp";
 import darkPaw from "../assets/footprint-dark.webp";
-import { MenuIcon, MoonIcon } from "@heroicons/react/solid";
+import { MoonIcon } from "@heroicons/react/solid";
 
 interface Props {
 	theme: string;
@@ -11,13 +10,13 @@ function Header(props: Props) {
 	return (
 		<div className="shadow-sm bg-white/30 sticky top-0 backdrop-blur-md">
 			<div className="flex justify-between p-4 lg:py-2 max-w-2xl lg:max-w-3xl mx-auto">
-				<div className="flex px-2 items-center">
+				<div className="flex px-2 items-center group">
 					<img
-						className="h-6 lg:h-5 hover:cursor-pointer hover:rotate-12 transition ease-in-out duration-300"
+						className="h-6 w-6 lg:h-5 lg:w-5 group-hover:cursor-pointer group-hover:rotate-12 transition ease-in-out duration-300"
 						src={props.theme === "dark" ? darkPaw : paw}
 						alt=""
 					/>
-					<div className="text-xl lg:text-lg font-semibold hover:cursor-pointer dark:text-white">
+					<div className="text-xl lg:text-lg font-semibold hover:cursor-pointer dark:text-white group">
 						Wilfredo Flores
 					</div>
 				</div>
