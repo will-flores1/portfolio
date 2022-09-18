1e00debc0d6943e2280eb6ac9f8eef629ced8433
wilfredo-flores <willflores1221@gmail.com>
2022-09-18 11:49:33 -0400

    Fix projects

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,18 +1,9 @@
-function Work() {
+import React from "react";
+
+function Work(props: any) {
 	return (
-		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 text-center dark:text-white mb-10">
-			<a href="" target="_blank" rel="noreferrer" className="">
-				<img src="" alt="thumbnail" className="rounded-xl mb-4" />
-				<p>How to build a portfolio website</p>
-			</a>
-			<a href="" target="_blank" rel="noreferrer" className="">
-				<img src="" alt="thumbnail" className="rounded-xl mb-4" />
-				<p>How to build a portfolio website</p>
-			</a>
-			<a href="" target="_blank" rel="noreferrer" className="">
-				<img src="" alt="thumbnail" className="rounded-xl mb-4" />
-				<p>How to build a portfolio website</p>
-			</a>
+		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 text-center dark:text-white mt-3">
+			{props.children}
 		</div>
 	);
 }
