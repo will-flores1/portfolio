aeb2a0b92c6c9cd1c5d0882ad98641f2c6f656c5
wilfredo-flores <willflores1221@gmail.com>
2022-09-30 00:28:40 -0400

    Add margin auto

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Projects(props: any) {
 	return (
-		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 text-center dark:text-white mt-3">
+		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 text-center dark:text-white mt-3 mx-auto">
 			{props.children}
 		</div>
 	);
