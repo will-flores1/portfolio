37379794f8485654e866fb6177199b1d0a21a676
Wilfredo Flores <willflores1221@gmail.com>
2022-10-24 00:51:25 -0400

    Fix project order

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -42,22 +42,24 @@ function WhoAmI() {
 			<div className="dark:text-white font-semibold text-2xl lg:text-xl border-black border-b-4 w-fit pb-1 tracking-wider dark:border-white">
 				Me in 10 seconds
 			</div>
-			<div className="mt-2 font-normal dark:text-white lg:text-base text-lg font-mono tracking-tighter">
-				- I'm a <span className="w-40">{age}</span> year old developer based in
-				Maryland, USA
-			</div>
-			<div className="mt-2 font-normal dark:text-white lg:text-base text-lg font-mono tracking-tighter">
-				- My introduction to programming was a C++ course at Montgomery College.
-			</div>
-			<div className="mt-2 font-normal dark:text-white lg:text-base text-lg font-mono tracking-tighter">
-				- Javascript is my language of choice for both quick scripts and web
-				development.
-			</div>
-			<div className="mt-2 font-normal dark:text-white lg:text-base text-lg font-mono tracking-tighter">
-				- I use React, Meta's framework, for creating SPA's.
-			</div>
-			<div className="mt-2 font-normal dark:text-white lg:text-base text-lg font-mono tracking-tighter">
-				- I speak English (native) and Spanish (proficient)
+			<div className="mt-2 font-normal dark:text-white lg:text-base text-lg">
+				<p className="py-2">
+					I'm a <span className="w-40">{age}</span> year old developer based in
+					Maryland, USA.
+				</p>
+				<p className="py-2">
+					My introduction to programming was a C++ course at Montgomery College.
+				</p>
+				<p className="py-2">
+					Javascript is my language of choice for both quick scripts and web
+					development.
+				</p>
+				<p className="py-2">
+					I use React, Meta's framework, for creating SPA's.
+				</p>
+				<p className="py-2">
+					I speak English (native) and Spanish (proficient).
+				</p>
 			</div>
 			{/* <a
 				href="https://www.wilfredoflores.dev/"
@@ -73,20 +75,13 @@ function WhoAmI() {
 			<Projects>
 				{/* OBS Plug-in */}
 				<ProjectTemplate
-					title="Timer OBS plug-in"
+					title="Countdown Timer for OBS"
 					description="Countdown Stream Timer/Clock for OBS"
 					demoLink=""
 					githubLink="https://github.com/will-flores1/Countdown-obs-plugin"
 					demoImg="./assets/timer-obs-plugin-demo.jpg"
 				/>
-				{/* Twitter */}
-				<ProjectTemplate
-					title="Twitter Feed Clone"
-					description="Sign in, tweet, and comment."
-					demoLink="https://twitter-2-0-nu.vercel.app/"
-					githubLink="https://github.com/will-flores1/twitter-2.0"
-					demoImg="./assets/twitter-demo.webp"
-				/>
+				{/* Sort Algorithms Visualizer */}
 				<ProjectTemplate
 					title="Sort Algorithms Visualizer"
 					description="Sorting algorithm visualization tool."
@@ -96,7 +91,7 @@ function WhoAmI() {
 				/>
 				{/* Local Library */}
 				<ProjectTemplate
-					title="Library App"
+					title="CRUD Library App"
 					description="Book tracking web application."
 					demoLink="https://library-app-navy.vercel.app/"
 					githubLink="https://github.com/will-flores1/Library-App"
@@ -104,13 +99,20 @@ function WhoAmI() {
 				/>
 				{/* Spotify API React */}
 				<ProjectTemplate
-					title="Spotify API React"
+					title="Spotify Web API App"
 					description="Spotify API categories, playlists, tracks UI"
 					demoLink="https://spotify-api-react.vercel.app/"
 					githubLink="https://github.com/will-flores1/spotify-api-react"
 					demoImg="./assets/spotify-demo.webp"
 				/>
-				{/* Sort Algorithms Visualizer */}
+				{/* Twitter */}
+				<ProjectTemplate
+					title="Twitter Clone"
+					description="Sign in, tweet, and comment."
+					demoLink="https://twitter-2-0-nu.vercel.app/"
+					githubLink="https://github.com/will-flores1/twitter-2.0"
+					demoImg="./assets/twitter-demo.webp"
+				/>
 			</Projects>
 			<div className="mt-6 font-semibold text-2xl lg:text-xl border-black border-b-4 w-fit pb-1 tracking-wide dark:text-white dark:border-white ">
 				On the web
