6ede1747d41a47587733d4c5255aea2dc91ee5c5
Wilfredo Flores <willflores1221@gmail.com>
2022-10-25 08:25:16 -0400

    Fix prop types

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+interface Props {
+	children: JSX.Element | JSX.Element[];
+}
 
-function Projects(props: any) {
+function Projects(props: Props) {
 	return (
 		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 text-center dark:text-white mt-3 mx-auto">
 			{props.children}
