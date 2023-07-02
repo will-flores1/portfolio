f1aee6ffc87287a80aaa8db8137ca1a592c75aa1
wilfredo-flores <willflores1221@gmail.com>
2023-07-02 03:52:49 -0400

    Add tags

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,60 +1,67 @@
 import ProjectTemplate from "./ProjectTemplate";
+import { useState } from "react";
+import projects from "../data/projects.json";
 
 interface Props {
 	children: JSX.Element | JSX.Element[];
 }
-
 function Projects() {
+	const [currentTag, setCurrentTag] = useState<string>("All");
+
+	const [tags, setTags] = useState<string[]>([
+		"All",
+		"TOP",
+		"React",
+		"Javascript",
+		"Svelte",
+		"Web App",
+		"Typescript",
+		"Website",
+		"Astro",
+		"Game",
+	]);
+
 	return (
-		<div className="grid grid-cols-1 gap-8 sm:grid-cols-2 text-center dark:text-white mt-3 mx-auto">
-			{/* Five O Autocare Mechanic Services Website */}
-			<ProjectTemplate
-				title="Rockville Mechanic"
-				description="Custom website services. Built w/ Astro, Svelte, & SASS."
-				demoLink="https://www.fiveoautocare.com/"
-				githubLink="https://github.com/will-flores1/mechanic-rockville"
-				demoImg="./assets/mechanic.jpg"
-			/>
-			{/* Pro Auto Body Services Website */}
-			<ProjectTemplate
-				title="Rockville Auto Body"
-				description="Custom website services. Built with Astro & Svelte."
-				demoLink="https://www.autobodyservicerockville.com"
-				githubLink="https://github.com/will-flores1/AutoBody-Rockville"
-				demoImg="./assets/autobody.jpg"
-			/>
-			{/* OBS Plug-in */}
-			<ProjectTemplate
-				title="Countdown Timer"
-				description="Countdown Stream Timer/Clock for Open Broadcast Software (OBS)"
-				demoLink=""
-				githubLink="https://github.com/will-flores1/Countdown-obs-plugin"
-				demoImg="./assets/timer-obs-plugin-demo.jpg"
-			/>
-			{/* Local Library */}
-			<ProjectTemplate
-				title="Library App"
-				description="Book tracking web application."
-				demoLink="https://library-app-navy.vercel.app/"
-				githubLink="https://github.com/will-flores1/Library-App"
-				demoImg="./assets/library-app-demo.jpg"
-			/>
-			{/* Spotify API React */}
-			<ProjectTemplate
-				title="Spotify Web App"
-				description="Interface to play w/ categories, playlists, tracks using the Spotify API"
-				demoLink="https://spotify-api-react.vercel.app/"
-				githubLink="https://github.com/will-flores1/spotify-api-react"
-				demoImg="./assets/spotify-demo.webp"
-			/>
-			{/* Twitter */}
-			<ProjectTemplate
-				title="Twitter Clone"
-				description="Interface clone using Tailwindcss. Features include sign in, tweet, and comment."
-				demoLink="https://twitter-2-0-nu.vercel.app/"
-				githubLink="https://github.com/will-flores1/twitter-2.0"
-				demoImg="./assets/twitter-demo.webp"
-			/>
+		<div>
+			<div className="flex gap-2 whitespace-nowrap scroll-smooth overflow-y-scroll no-scrollbar mt-10 font-semibold dark:text-white text-black/90">
+				Tags:
+				{tags.map((tag) => (
+					<button
+						className={
+							currentTag === tag
+								? "bg-gray-300 dark:bg-gray-500 dark:text-white text-black/90 px-3 py-1 rounded-md text-sm font-semibold"
+								: "bg-gray-200 dark:bg-gray-700 dark:text-white text-black/90 px-3 py-1 rounded-md text-sm font-semibold hover:first-letter:underline hover:bg-gray-300 dark:hover:bg-gray-500"
+						}
+						key={tag}
+						onClick={() => {
+							setCurrentTag(tag);
+							console.log("currentTag:" + currentTag);
+						}}
+					>
+						{tag}
+					</button>
+				))}
+			</div>
+
+			<div className="grid grid-cols-1 gap-8 sm:grid-cols-2 text-center dark:text-white mt-10 mx-auto">
+				{projects.map((project) => {
+					if (currentTag === "All" || project.tags.includes(currentTag)) {
+						return (
+							<ProjectTemplate
+								key={project.title}
+								githubLink={project.githubLink}
+								demoImg={project.demoImg}
+								title={project.title}
+								description={project.description}
+								demoLink={project.demoLink}
+								tags={project.tags}
+								currentTag={currentTag}
+							/>
+						);
+					}
+					return null;
+				})}
+			</div>
 		</div>
 	);
 }
