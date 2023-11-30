144564088d95ea70bdf3c46345aad9bfaf1e8276
wilfredo-flores <willflores1221@gmail.com>
2023-11-30 03:34:45 -0500

    Fix: edit format

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -30,51 +30,45 @@ function WhoAmI() {
 	return (
 		<div
 			className="flex flex-col mx-auto max-w-3xl lg:max-w-3xl px-10 lg:px-20"
-			id="fade"
-		>
+			id="fade">
 			<main>
 				<section>
-					<div className="lg:flex lg:justify-between mt-1">
+					{/* <div className="lg:flex lg:justify-between mt-1">
 						<div>
 							<div className="mt-5 dark:text-white font-bold tracking-wider lg:text-3xl text-4xl text-black/90">
 								Wilfredo Flores
 							</div>
-							{/* <div className="text-black/70 dark:text-white text-lg lg:text-base">
+							<div className="text-black/70 dark:text-white text-lg lg:text-base">
 								Programmer
-							</div> */}
+							</div>
 						</div>
 						<img
 							src="./profile-pic.webp"
 							className="lg:mx-0 h-40 w-40 mx-auto my-4 rounded-full"
 							alt="profile"
 						/>
-					</div>
+					</div> */}
 					<div className="dark:text-white font-semibold text-5xl lg:text-6xl border-black border-b-4 w-fit pb-1 tracking-wider dark:border-white mt-16">
 						Me in 10 seconds
 					</div>
 					<div className="mt-2 font-normal dark:text-white lg:text-base text-lg font-mono tracking-tighter">
 						<p className="py-2">
-							I'm a <span className="w-40">{age}</span> year old developer
+							I'm a <span className="w-40">{age}</span> year old
+							developer
 						</p>
 						<p className="py-2">
-							<strong>Languages:</strong> Javascript/Typescript, HTML + CSS,
-							Python
+							<strong>Languages:</strong> Javascript, HTML + CSS,
+							Express, Mongoose
 						</p>
 						<p className="py-2">
-							<strong>Frontend:</strong> React, Svelte, Wordpress
+							<strong>Frameworks:</strong> React, WordPress, Bootstrap
 						</p>
-						<p className="py-2">
-							<strong>Styling:</strong> Tailwindcss, SASS, Bootstrap
-						</p>
-						<p className="py-2">
-							<strong>Backend:</strong> Node, Express, Mongoose, MongoDB, MySQL
-						</p>
+						<strong>Databases:</strong> MongoDB, SQLite
 					</div>
 				</section>
-				<button
+				{/* <button
 					className="bg-emerald-600 w-fit flex mx-auto mt-16 mb-10 py-3 px-6 rounded-md text-white hover:cursor-pointer hover:bg-emerald-700 transition ease-in-out duration-150 font-bold"
-					onClick={handleClick}
-				>
+					onClick={handleClick}>
 					Random Project
 				</button>
 				{getRandomProject && (
@@ -86,8 +80,10 @@ function WhoAmI() {
 						demoLink={projects[i].demoLink}
 						tags={projects[i].tags}
 					/>
-				)}
-				<div className="mt-6 font-semibold text-5xl lg:text-6xl border-black border-b-4 w-fit pb-1 tracking-wider dark:text-white dark:border-white pt-16">
+				)} */}
+				<div
+					id="projects"
+					className="mt-6 font-semibold text-5xl lg:text-6xl border-black border-b-4 w-fit pb-1 tracking-wider dark:text-white dark:border-white pt-16">
 					Projects
 				</div>
 				<Projects />
@@ -99,10 +95,6 @@ function WhoAmI() {
 				<HyperLink
 					img="./assets/github.svg"
 					link="https://github.com/will-flores1"
-				/>
-				<HyperLink
-					img="./assets/twitter.svg"
-					link="https://twitter.com/will_flores1"
 				/>
 			</footer>
 		</div>
