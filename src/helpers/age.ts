4609b2cd12db76e1a7d0ab4f1b19981d683419ce
Wilfredo Flores <willflores1221@gmail.com>
2024-06-19 14:45:29 -0400

    Clean gh-page branch

--- src/helpers/age.ts
+++
@@ -1,7 +0,0 @@
-export const getAge = () => {
-	let date = new Date();
-	let time = date.valueOf() - new Date(914290179349).valueOf();
-	let age = time / (1000 * 60 * 60 * 24 * 365.25);
-	let formatTime = age.toString().substring(0, 12);
-	return formatTime;
-};
