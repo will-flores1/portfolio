a93bc3f21c5ddea10164e97cd4b6a2b174875538
Wilfredo Flores <willflores1221@gmail.com>
2024-06-19 14:15:16 -0400

    Fix: title & image props

--- src/components/HyperLink.tsx
+++ src/components/HyperLink.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 interface Props {
 	link: string;
-	img: string;
+	img: string | undefined;
+	title: string;
 }
 
 function HyperLink(props: Props) {
@@ -11,15 +12,17 @@ function HyperLink(props: Props) {
 			href={props.link}
 			target="_blank"
 			rel="noreferrer"
-			className="mt-3 flex items-center hover:bg-teal-100 py-2 px-4 rounded-md transition duration-300 ease-in-out w-fit text-lg lg:text-base group"
-		>
-			<img
-				src={props.img}
-				alt=""
-				className="h-7 w-7 lg:h-6 lg:w-6 fill-current pr-2"
-			/>
+			className="mt-3 flex items-center hover:bg-teal-100 py-2 px-4 rounded-md transition duration-300 ease-in-out w-fit text-lg lg:text-base group">
+			{props.img ? (
+				<img
+					src={props.img}
+					alt=""
+					className="h-7 w-7 lg:h-6 lg:w-6 fill-current pr-2"
+				/>
+			) : null}
 			<div className="font-bold text-moonTeal group-hover:underline">
-				<span className="font-light">@</span>will-flores1
+				<span className="font-light">@</span>
+				{props.title}
 			</div>
 		</a>
 	);
